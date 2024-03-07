d980beb4c2eb0054175b1cc78e4ecf44744d18bf
Guilherme Murata <123330836+GuilhermeMurata@users.noreply.github.com>
2024-03-07 16:18:50 -0300

    First commit

---
+++ main.js
@@ -0,0 +1,39 @@
+const form = document.getElementById('form-cadastro');
+const email = [];
+const senha = [];
+
+let linhas = '';
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    adicionaLinha();
+    atualizaTabela();
+});
+
+function adicionaLinha() {
+    const inputEmailUsuario = document.getElementById('email-usuario');
+    const inputSenhaUsuario = document.getElementById('senha-usuario');
+
+    if (email.includes(inputEmailUsuario.value)) {
+        alert(`Contato ${inputEmailUsuario} ja foi cadastrado`);
+    } else {
+        email.push(inputEmailUsuario.value);
+        senha.push(parseFloat(inputSenhaUsuario.value));
+
+        let linha = '<tr>';
+        linha += `<td>${inputEmailUsuario.value}</td>`;
+        linha += `<td>${inputSenhaUsuario.value}</td>`;
+        linha += '</tr>';
+
+        linhas += linha;
+    }
+
+    inputEmailUsuario.value = '';
+    inputSenhaUsuario.value = '';
+}
+
+function atualizaTabela() {
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
